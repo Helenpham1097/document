54fbf69e3214691fe2729dc077705478edbda3d3
Tim Molloy <tim.molloy@sitehost.co.nz>
2021-01-27 17:28:21 +1300

    Add layout to RSS feed routes

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::statamic('/blog/feed', 'blog.feed', [
     'content_type' => 'xml',
-    'layout' => null
+    'layout' => 'blog/feed_layout'
 ]);
 
 Route::statamic('/blog/feed/index.xml', 'blog.feed', [
     'content_type' => 'xml',
-    'layout' => null
+    'layout' => 'blog/feed_layout'
 ]);
