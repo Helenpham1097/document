8b0a72e5ee6713568c21343268d4de1f309d1606
Ariel Cerda <ariel@sitehost.co.nz>
2023-11-07 11:10:57 +1300

    Updated gulp file to match the process of MyHost and SiteHost

--- gulpfile.js
+++ gulpfile.js
@@ -7,113 +7,94 @@ const imagemin = require("gulp-imagemin");
 const newer = require("gulp-newer");
 const plumber = require("gulp-plumber");
 const postcss = require("gulp-postcss");
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
 
 function clean() {
-   return del(['public/css', 'public/js']);
+	return del(["public/css"]);
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
+	function styles() {
+		return gulp.src("./resources/css/main.css").pipe(plumber()).pipe(postcss()).pipe(gulp.dest("public/css"));
+	}
 
-   function styles() {
+	// function scripts() {
+	//    return gulp
+	//       .src('./resources/js/**/*.js')
+	//       .pipe(plumber())
+	//       .pipe(concat('main.js'))
+	//       .pipe(uglify())
+	//       .pipe(gulp.dest('public/js'))
+	// }
 
-      return gulp
-         .src("./resources/css/main.css")
-         .pipe(plumber())
-         .pipe(postcss())
-         .pipe(gulp.dest("public/css"))
-   }
-   
-   function scripts() {
-      return gulp
-         .src('./resources/js/**/*.js')
-         .pipe(plumber())
-         .pipe(concat('main.js'))
-         .pipe(uglify())
-         .pipe(gulp.dest('public/js'))
-   }
+	const build = gulp.series(clean, gulp.parallel(styles));
+	// export tasks
+	exports.build = build;
+} else {
+	// BrowserSync
+	function browserSync(done) {
+		browsersync.init({
+			proxy: "webslice-website.test",
+		});
+		done();
+	}
 
-   const build = gulp.series(clean, gulp.parallel(styles, scripts));
-   // export tasks
-   exports.build = build;
-} 
+	// BrowserSync Reload
+	function browserSyncReload(done) {
+		browsersync.reload();
+		done();
+	}
 
-else {
+	function html() {
+		return gulp.src("./resources/views/**/*").pipe(browsersync.stream());
+	}
 
-   // BrowserSync
-   function browserSync(done) {
-      browsersync.init({
-         proxy: "webslice-website.test"
-      });
-      done();
-   }
+	function images() {
+		return gulp
+			.src("./resources/img/**/*")
+			.pipe(newer("./public/img"))
+			.pipe(
+				imagemin([
+					imagemin.gifsicle({ interlaced: true }),
+					imagemin.mozjpeg({ quality: 75, progressive: true }),
+					imagemin.optipng({ optimizationLevel: 5 }),
+					imagemin.svgo({
+						plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+					}),
+				])
+			)
+			.pipe(gulp.dest("./public/img"));
+	}
 
-   // BrowserSync Reload
-   function browserSyncReload(done) {
-      browsersync.reload();
-      done();
-   }
+	function styles() {
+		return gulp.src("./resources/css/main.css").pipe(plumber()).pipe(postcss()).pipe(gulp.dest("public/css")).pipe(browsersync.stream());
+	}
 
-   function html() {
-      return gulp
-         .src("./resources/views/**/*")
-         .pipe(browsersync.stream());
-   }
+	// function scripts() {
+	//    return gulp
+	//       .src('./resources/js/**/*.js')
+	//       .pipe(plumber())
+	//       .pipe(concat('main.js'))
+	//       .pipe(uglify())
+	//       .pipe(gulp.dest('public/js'))
+	//       .pipe(browsersync.stream());
+	// }
 
-   function images() {
-      return gulp
-         .src("./resources/img/**/*")
-         .pipe(newer("./public/img"))
-         .pipe(
-            imagemin([
-               imagemin.gifsicle({ interlaced: true }),
-               imagemin.mozjpeg({ quality: 75, progressive: true }),
-               imagemin.optipng({ optimizationLevel: 5 }),
-               imagemin.svgo({
-                  plugins: [
-                     { removeViewBox: true },
-                     { cleanupIDs: false }
-                  ]
-               })
-            ])
-         )
-         .pipe(gulp.dest("./public/img"));
-   }
+	// Watch files
+	function watchFiles() {
+		gulp.watch("./resources/views/**/*", gulp.series(styles, html));
+		gulp.watch("./resources/css/**/*", styles);
+		// gulp.watch("./resources/js/**/*", scripts);
+		gulp.watch("./resources/img/**/*", images);
+		gulp.series(browserSyncReload);
+	}
 
-   function styles() {
-      return gulp
-         .src("./resources/css/main.css")
-         .pipe(plumber())
-         .pipe(postcss())
-         .pipe(gulp.dest("public/css"))
-         .pipe(browsersync.stream());
-   }
-   
-   function scripts() {
-      return gulp
-         .src('./resources/js/**/*.js')
-         .pipe(plumber())
-         .pipe(concat('main.js'))
-         .pipe(uglify())
-         .pipe(gulp.dest('public/js'))
-         .pipe(browsersync.stream());
-   }
+	// define complex tasks
+	const build = gulp.series(clean, gulp.parallel(html, styles, images));
+	const watch = gulp.parallel(watchFiles, browserSync);
 
-   // Watch files
-   function watchFiles() {
-      gulp.watch("./resources/views/**/*", gulp.series(styles, html));
-      gulp.watch("./resources/css/**/*", styles);
-      gulp.watch("./resources/js/**/*", scripts);
-      gulp.watch("./resources/img/**/*", images);
-      gulp.series(browserSyncReload);
-   }
-
-   // define complex tasks
-   const build = gulp.series(clean, gulp.parallel(html, styles, images, scripts));
-   const watch = gulp.parallel(watchFiles, browserSync);
-
-   // export tasks
-   exports.build = build;
-   exports.watch = watch;
+	// export tasks
+	exports.build = build;
+	exports.watch = watch;
 }
