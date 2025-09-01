b3cb4106fa28c8f3be174623700c06940fef9408
Ben Lilley <ben@sitehost.co.nz>
2025-09-01 15:58:38 +1200

    fix: restore the creation of the .env file in the build script as needed every Webslice deploy.

--- .webslice/build.sh
+++ .webslice/build.sh
@@ -5,6 +5,7 @@ echo "Starting Build Script"
 # Print command output for each command and exit immediately if any command fails (https://man7.org/linux/man-pages/man1/set.1p.html)
 set -ex
 
+cp .env.example .env
 COMPOSER_PROCESS_TIMEOUT=2000 composer install --no-interaction -vvv --no-dev --prefer-dist --optimize-autoloader
 npm ci --cache .npm --prefer-offline
 npm run build:prod
